7ac63d93c8d5511960707f5bb8481e83558938b0
i2tsuki <mac-daisuki@live.jp>
2019-12-19 11:06:14 +0900

    Create an IMAP session

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate imap;
+extern crate native_tls;
+
 fn main() {
+    let domain = "imap.example.com";
+    let user = "";
+    let password = "";
+
+    let tls = native_tls::TlsConnector::builder().build().unwrap();
+
+    let client = imap::connect((domain, 993), domain, &tls).unwrap();
+
+    let mut imap_session = client.login(user, password).unwrap();
+
     println!("Hello, world!");
 }
