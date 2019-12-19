05399ec32c79073aac2ac4cb49dfdc9e588c1bc5
i2tsuki <mac-daisuki@live.jp>
2019-12-19 11:18:58 +0900

    Make port as variable to avoid hard courting

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() {
     let domain = "imap.example.com";
     let user = "";
     let password = "";
+    let port = 993;
 
     let tls = native_tls::TlsConnector::builder().build().unwrap();
 
-    let client = imap::connect((domain, 993), domain, &tls).unwrap();
+    let client = imap::connect((domain, port), domain, &tls).unwrap();
 
     let mut imap_session = client.login(user, password).unwrap();
 
