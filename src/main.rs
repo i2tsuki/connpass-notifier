3935ba746a1d22be8a5334cac21a9363b1fbbefc
i2tsuki <mac-daisuki@live.jp>
2019-12-19 12:17:35 +0900

    Give IMAP client params using environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate imap;
 extern crate native_tls;
 
+use std::env;
+
 fn main() {
-    let domain = "imap.example.com";
-    let user = "";
-    let password = "";
-    let port = 993;
+    let domain = env::var("IMAP_DOMAIN").unwrap();
+    let port = env::var("IMAP_PORT").unwrap().parse::<u16>().unwrap();
+    let user = env::var("IMAP_USER").unwrap();
+    let password = env::var("IMAP_PASSWORD").unwrap();
 
     let tls = native_tls::TlsConnector::builder().build().unwrap();
 
-    let client = imap::connect((domain, port), domain, &tls).unwrap();
+    let client = imap::connect((domain.as_str(), port), &domain, &tls).unwrap();
 
     let mut imap_session = client.login(user, password).unwrap();
 
