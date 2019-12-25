9b4cab0d6fb5606c4f0d3b04812d9baaf2cfd484
i2tsuki <mac-daisuki@live.jp>
2019-12-26 00:11:00 +0900

    Add types in declaration

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,33 @@ extern crate native_tls;
 extern crate regex;
 extern crate time;
 
+use std::collections::HashSet;
 use std::env;
 use std::io::{Read, Write};
 
 use chrono::prelude::*;
-use imap::types::Seq;
+use imap::types::*;
 use mailparse::*;
+use native_tls::{TlsConnector, TlsStream};
 use regex::Regex;
+use std::net::TcpStream;
 use time::Duration;
 
 fn main() {
-    let domain = env::var("IMAP_DOMAIN").expect("IMAP_DOMAIN is not given");
-    let port = env::var("IMAP_PORT")
+    let domain: String = env::var("IMAP_DOMAIN").expect("IMAP_DOMAIN is not given");
+    let port: u16 = env::var("IMAP_PORT")
         .expect("IMAP_PORT is not given")
         .parse::<u16>()
         .expect("IMAP_PORT is not positive number");
-    let user = env::var("IMAP_USER").expect("IMAP_USER is not given");
-    let password = env::var("IMAP_PASSWORD").expect("IMAP_PASSWORD is not given");
+    let user: String = env::var("IMAP_USER").expect("IMAP_USER is not given");
+    let password: String = env::var("IMAP_PASSWORD").expect("IMAP_PASSWORD is not given");
 
-    let chunk = 10;
+    let chunk: usize = 10;
 
-    let tls = native_tls::TlsConnector::builder().build().unwrap();
+    let tls: TlsConnector = native_tls::TlsConnector::builder().build().unwrap();
 
-    let client = imap::connect((domain.as_str(), port), &domain, &tls).unwrap();
+    let client: imap::Client<TlsStream<TcpStream>> =
+        imap::connect((domain.as_str(), port), &domain, &tls).unwrap();
 
     let mut imap_session = client.login(user, password).unwrap();
 
@@ -33,7 +37,7 @@ fn main() {
 
     let since: DateTime<Local> = Local::now() - Duration::hours(24 * 30);
 
-    let sequences = imap_session
+    let sequences: HashSet<Seq> = imap_session
         .search(format!(
             "FROM no-reply@connpass.com SINCE {}",
             since.format("%d-%b-%Y")
@@ -51,25 +55,25 @@ fn main() {
 }
 
 fn get_message_subject<T: Read + Write>(imap_session: &mut imap::Session<T>, seq: Seq) {
-    let message_id = &seq.to_string();
-    let messages = imap_session.fetch(message_id, "RFC822").unwrap();
+    let message_id: &str = &seq.to_string();
+    let messages: ZeroCopy<Vec<Fetch>> = imap_session.fetch(message_id, "RFC822").unwrap();
     imap_session.store(message_id, "-FLAGS (\\Seen)").unwrap();
 
-    let message = if let Some(m) = messages.iter().next() {
+    let message: &Fetch = if let Some(m) = messages.iter().next() {
         m
     } else {
         return;
     };
 
-    let body = message.body().expect("message did not have a body!");
-    let body = std::str::from_utf8(body).expect("message was not valid utf-8");
+    let body: &[u8] = message.body().expect("message did not have a body!");
+    let body: &str = std::str::from_utf8(body).expect("message was not valid utf-8");
 
-    let parsed = parse_mail(body.as_bytes()).unwrap();
+    let parsed: mailparse::ParsedMail = parse_mail(body.as_bytes()).unwrap();
 
-    let date = parsed.headers.get_first_value("Date").unwrap().unwrap();
-    let subject = parsed.headers.get_first_value("Subject").unwrap().unwrap();
+    let date: String = parsed.headers.get_first_value("Date").unwrap().unwrap();
+    let subject: String = parsed.headers.get_first_value("Subject").unwrap().unwrap();
 
-    let re = Regex::new(r"^.*さんが.*に参加登録しました。$").unwrap();
+    let re: Regex = Regex::new(r"^.*さんが.*に参加登録しました。$").unwrap();
     if re.is_match(&subject) {
         println!("{:<32}: {}", date, subject);
         imap_session
@@ -77,7 +81,7 @@ fn get_message_subject<T: Read + Write>(imap_session: &mut imap::Session<T>, seq
             .unwrap();
     }
 
-    let re = Regex::new(r"^.*がイベント.*を公開しました$").unwrap();
+    let re: Regex = Regex::new(r"^.*がイベント.*を公開しました$").unwrap();
     if re.is_match(&subject) {
         println!("{:<32}: {}", date, subject);
         imap_session
